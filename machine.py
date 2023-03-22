5df70990c00cec5b4207bfa7341e335a78919798
SsShu022 <shuliak.pn@ucu.edu.ua>
2023-03-22 09:16:39 +0200

    added is_empty method

--- machine.py
+++ machine.py
@@ -1,15 +1,14 @@
 """Machine"""
-
-class VendingMachine:
-    def __init__(self) -> None:
-        pass
-
+    
 class TextMachine:
-    def __init__(self) -> None:
-        pass
+    def __init__(self, text1:tuple, text2:tuple) -> None:
+        self.text1 = text1
+        self.text2 = text2
 
     def is_empty(self):
-        pass
+        if len(self.text1) == 0 and len(self.text2) == 0:
+            return True
+        return False
 
     def texts_count(self):
         pass
@@ -25,3 +24,7 @@ class TextMachine:
 
     def railway_station_machine(self):
         pass
+
+class VendingMachine(TextMachine):
+    def __init__(self, text1: tuple, text2: tuple) -> None:
+        super().__init__(text1, text2)
