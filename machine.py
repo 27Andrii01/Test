74622475ef34002cf875cbbad078b02dd3f29119
SsShu022 <shuliak.pn@ucu.edu.ua>
2023-03-22 09:04:24 +0200

    added methods for text machine

--- machine.py
+++ machine.py
@@ -1,7 +1,27 @@
 """Machine"""
 
 class VendingMachine:
-    pass
+    def __init__(self) -> None:
+        pass
 
 class TextMachine:
-    pass
+    def __init__(self) -> None:
+        pass
+
+    def is_empty(self):
+        pass
+
+    def texts_count(self):
+        pass
+
+    def still_owe(self):
+        pass
+
+    def insert_money(self):
+        pass
+
+    def stock_machine(self):
+        pass
+
+    def railway_station_machine(self):
+        pass
