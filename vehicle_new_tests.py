8526a218cd33e286512ba250f4c088ae948ffc3a
Drakonchyk <63148058+Drakonchyk@users.noreply.github.com>
2023-03-22 09:46:39 +0200

    created file for new tests

---
+++ vehicle_new_tests.py
@@ -0,0 +1,60 @@
+from vehicle import *
+
+def test_vehicle():
+    """
+    Print Done if all tests passed
+    """
+    print("Testing Vehicle class...", end="")
+    v3 = Vehicle('Honda', 3)
+    # check if main information about the vehicle and passengers is correct
+    assert isinstance(v3, Vehicle) is True
+    assert str(v3) == "Honda holds 3: [], free 3"
+    assert v3.is_empty() is True
+    p3 = Passenger('Olenka')
+    assert str(p3) == "Olenka is passenger of ..."
+    p4 = Passenger('Maksym')
+    p5 = Passenger('Nastia')
+    assert isinstance(p4, Passenger) is True
+    # can't remove a passenger from an empty bus or vehicle
+    assert v3.remove_passenger() is False
+    # passengers can be added
+    assert v3.add_passenger(p3) is True
+    assert str(v3) == "Honda holds 3: ['Olenka'], free 2"
+    assert v3.add_passenger(p4) is True
+    assert str(v3) == "Honda holds 3: ['Olenka', 'Maksym'], free 1"
+    assert v3.add_passenger(p5) is True
+    assert str(v3) == "Honda holds 3: ['Olenka', 'Maksym', 'Nastia'], free 0"
+    p6 = Passenger('Katia')
+    # can't add passengers past the max capacity
+    assert v3.add_passenger(p6) is False
+    assert str(v3) == "Honda holds 3: ['Olenka', 'Maksym', 'Nastia'], free 0"
+    assert v3.passengers == ['Olenka', 'Maksym', 'Nastia']
+    assert v3.remove_passenger() == p5
+    assert v3.add_passenger(p6) is True
+    assert str(v3) == "Honda holds 3: ['Olenka', 'Maksym', 'Katia'], free 0"
+    # some cars do not have passenger places at all
+    v4 = Vehicle('Bugatti', 0)
+    assert str(v4) == "Bugatti holds 0: [], free 0"
+    assert p5.place is None
+    assert str(p5) == "Nastia is passenger of ..."
+    assert v4.add_passenger(p5) is False
+    assert v4.remove_passenger() is False
+    assert v4.is_empty() is True
+    assert p5.place is None
+    assert str(p5) == "Nastia is passenger of ..."
+    # the last car
+    v5 = Vehicle('BMW', 2)
+    assert str(v5) == "BMW holds 2: [], free 2"
+    assert v5.add_passenger(p5) is True
+    assert str(v5) == "BMW holds 2: ['Nastia'], free 1"
+    assert p5.place == v5
+    assert str(p5) == "Nastia is passenger of BMW"
+    # if a person is already in a vehicle and you want him/her to be in another one, he/she changes
+    # her place to a new car and is removed from the previous one
+    assert p6.place == v3
+    assert v5.add_passenger(p6) is True
+    assert str(v5) == "BMW holds 2: ['Nastia', 'Katia'], free 0"
+    assert p6.place == v5
+    assert str(v3) == "Honda holds 3: ['Olenka', 'Maksym'], free 1"
+    print("Done!")
+test_vehicle()
